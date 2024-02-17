b80f72509c9c28333b09ae081b0cdd83c769c01b
IRahman2024 <stormfuryultra784@gmail.com>
2024-02-17 16:37:06 +0600

    toggole done

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,16 @@
+// function play(){
+//     //step-1: hide the home screen. in section
+//     const home = document.getElementById('home');
+//     home.classList.add('hidden');
+
+
+//     //step-2: show the playground
+//     const playground = document.getElementById('play-ground');
+//     playground.classList.remove('hidden');
+//     console.log('play start now')
+// }
+
+function play(){
+    hideElementById('home');//section hide er jonno onno function e pathay disse
+    showElementById('play-ground');
+}
